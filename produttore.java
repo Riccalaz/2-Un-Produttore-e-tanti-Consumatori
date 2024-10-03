7683b6a4196e97cca37839a9ee4e53c7bd8ac079
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2024-10-03 19:57:58 +0200

    Add files via upload

---
+++ produttore.java
@@ -0,0 +1,43 @@
+package unptantic;
+
+import java.util.Random;
+
+public class produttore extends Thread{
+	buffer b;
+	
+	public produttore(buffer buf) {
+		b=buf;
+	}
+	
+	public void run() {
+		Random ran = new Random();
+		
+		while(true) {
+			int r=ran.nextInt(2048)-1024;
+			
+			try {
+				b.ins(r);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(r>0) {
+				try {
+					Thread.sleep(r/100*50);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}else {
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+	}
+}
